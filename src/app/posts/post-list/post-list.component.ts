dcf61c9ab1dcaa5e047b874ce16963b5a54e13d9
somikdatta <developersomik@gmail.com>
2019-09-24 23:40:42 +0530

    Models created

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
+import { Post } from "../post.model";
 
 @Component({
   selector: "app-post-list",
@@ -6,7 +7,7 @@ import { Component, OnInit, Input } from "@angular/core";
   styleUrls: ["./post-list.component.css"]
 })
 export class PostListComponent implements OnInit {
-  @Input() posts = [];
+  @Input() posts: Post[] = [];
 
   constructor() {}
 
