f9992b89c9c102e058da699a7fee7206d7c963c7
somikdatta <developersomik@gmail.com>
2019-10-01 13:50:48 +0530

    Connected MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://somikdatta:8jJuDbh6Z08s5TrZ@cluster0-owthk.mongodb.net/test?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('Connected to MongoDB');
+  }).catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,10 +22,13 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS");
   next();
-})
+});
 
 app.post("/api/posts", (req, res, next) => {
-  const posts = req.body;
+  const posts = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log(posts);
   res.status(201).json({
     message: 'Post added successfully'
