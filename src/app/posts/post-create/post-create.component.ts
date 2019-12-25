11e2989d75966db4c13d24f2454ede233be1e482
Somik Datta <developersomik@gmail.com>
2019-12-25 19:38:53 +0530

    Edit post with image upload

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -46,12 +46,12 @@ export class PostCreateComponent implements OnInit {
             id: postData.post._id,
             title: postData.post.title,
             content: postData.post.content,
-            imagePath: null
+            imagePath: postData.post.imagePath
           };
           this.form.setValue({
             title: this.post.title,
-            content: this.post.content
-            // image: this.post.imagePath
+            content: this.post.content,
+            image: this.post.imagePath
           });
         });
       } else {
@@ -87,7 +87,8 @@ export class PostCreateComponent implements OnInit {
       this.postsService.updatePost(
         this.postId,
         this.form.value.title,
-        this.form.value.content
+        this.form.value.content,
+        this.form.value.image
       );
     }
     this.form.reset();
