3f7d4af7d0661b6249501a8516cd3ce5326d18ff
Somik Datta <developersomik@gmail.com>
2019-12-29 12:50:55 +0530

    1. refactored multer middleware 2. path changes

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ mongoose.connect("mongodb+srv://somikdatta:" + process.env.MONGO_ATLAS_PASS + "@
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join("images")));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
